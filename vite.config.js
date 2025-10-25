5ce004b796fe42c359b37961b257299e58a832ab
Prasadp20 <prasad.pathak5555@gmail.com>
2025-10-25 13:05:55 +0530

    Add Deployment Code

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Millets_Project/', // ← replace REPO_NAME with your GitHub repo name
   plugins: [react()],
 })
